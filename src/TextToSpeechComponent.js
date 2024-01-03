50339a9fe5fb5d6411631c8a9ecf1664a25c3448
KianIsNotAvailable <kianxw@gmail.com>
2024-01-03 10:13:41 +0000

    dotenv was bugging

--- src/TextToSpeechComponent.js
+++ src/TextToSpeechComponent.js
@@ -5,8 +5,8 @@ import AWS from 'aws-sdk';
 import Speaker from './images/speaker.png'
 
 AWS.config.update({
-  accessKeyId: 'AKIA2AHDZUT2P22KCUUE',
-  secretAccessKey: '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',
+  accessKeyId: process.env.REACT_APP_ACCESS_KEY,
+  secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY,
   region: 'eu-north-1',
 });
 
